32696ad2b7ae9e21c9cf0ac35362b82a526006c5
Bola Bally <bolabally@gmail.com>
2020-05-07 16:47:47 +0000

    addapi.php

--- database/migrations/2020_05_07_162409_create_products_table.php
+++ database/migrations/2020_05_07_162409_create_products_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateBlogsTable extends Migration
+class CreateProductsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,8 +13,10 @@ class CreateBlogsTable extends Migration
      */
     public function up()
     {
-        Schema::create('blogs', function (Blueprint $table) {
+        Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->text('detail');
             $table->timestamps();
         });
     }
@@ -26,6 +28,6 @@ class CreateBlogsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('blogs');
+        Schema::dropIfExists('products');
     }
 }
